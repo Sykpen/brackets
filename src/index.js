340c0787c66e9b60884087ebe4bad38e30d4106d
Sykpen <ser10potap@yandex.ru>
2020-03-22 19:25:02 +0300

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i <= bracketsConfig.length - 1;) {
+    let pattern = bracketsConfig[i].join('');
+    if (str.includes(pattern)) {
+        str = str.replace(pattern, '');
+        i = 0;
+    } else {
+        i = i + 1;
+    }
+}
+if (str.length == 0) {
+    return true;
+} else {
+    return false;
+}
 }
